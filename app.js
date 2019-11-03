1a8af3b7224e7a3d5844edd6a97c46a43760170d
brainee <zhengyong145@163.com>
2019-11-03 19:02:16 +0800

    run up /index

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var adminRouter = require('./routes/admin');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -36,6 +38,10 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+var server=app.listen(5388, function () {
+  console.log('Listening on '+server.address().port);
 });
 
 module.exports = app;
--- index.js
+++
@@ -1,8 +0,0 @@
-var app=require("./app/index");
-
-
-var server=app.listen(5388, function () {
-  console.log('Listening on '+server.address().port);
-});
-
-
