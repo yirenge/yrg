089db56890dfb4c7541b5bd5c26a8e75220bce77
zhengyong <zhengyong145@163.com>
2019-11-04 19:42:49 +0800

    run yrg

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var myconfig=require("./src/myconfig");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var adminRouter = require('./routes/admin');
+
 
 var app = express();
 
@@ -20,9 +22,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/admin', adminRouter);
+app.use(myconfig.baseUrl +'/', indexRouter);
+app.use(myconfig.baseUrl +'/users', usersRouter);
+app.use(myconfig.baseUrl +'/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
